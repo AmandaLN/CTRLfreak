b52ccd6ba6fde367bc07dd0b3c674e5774292383
rvasquez <rvasquez1@gmail.com>
2021-03-07 17:47:41 -0500

    changing the donuts charts

--- client/src/components/Reports/index.js
+++ client/src/components/Reports/index.js
@@ -1,265 +1,180 @@
 import React, { useEffect, useState } from "react";
 import { Bar, Doughnut, defaults } from "react-chartjs-2";
-import { Col, Row } from '../../components/Grid';
-import HR from '../HR';
+import { Col, Row } from "../Grid";
+import DoughnutChart from "../Doughnut";
+import HR from "../HR";
 import { List, ListItem } from "../List";
 import API from "../../utils/API";
 let totalGroceries = 0;
+let back = []
+let percentage = []
 
-
-defaults.global.legend.position = 'bottom'
+defaults.global.legend.position = "bottom";
 
 /* This is a very simple component.. it probably doesn't need to be a smart component at this point but you never know what's goingto happen in the future */
 
-function PublicRoute({budgets, expensesTotal}) {
+function Reports({ budgets, expensesTotal }) {
+const [budgetsbyType, setbudgetsbyType] = useState({});
+  const [budgetsGroceries, setGroceries] = useState(0);
+  const [budgetsUtilities, setUtilities] = useState(0);
+  const [budgetsSubscription, setSubscription] = useState(0);
+  const [budgetsTotal, setTotal] = useState(0);
 
-      
-    const [budgetsGroceries, setBudgetsGroceries] = useState([])
-    const [budgetsUtilities, setBudgetsUtilities] = useState([])
-    const [budgetsSubscription, setBudgetsSubscription] = useState([])
-    
-  
-    // Load all books and store them with setBooks
-     useEffect(() => {
-        console.log("useEffect")
-        fetch('api/users/user', {
-			credentials: 'include'
-			})
-			.then((res) => {
-				console.log(`response to authenticate ${res}`);
-				console.log(res, "yesyes")
-				return res.json(res)
-			})
-			.then(  data => {
-                console.log(data, "checkdata")
-               let userName = {
-                    user: data.username
-                }
-                console.log(userName, "username")
-        API.getExpensesbyType("Groceries", userName)
-        .then(res => {
-            console.log("nada")
-          if (res) {
-            console.log(res.data, "checking  groceries")
-            
-            // console.log(expensesTotal, "totalexpenses")
-            // setBudgetsGroceries(res.data[0].expenses)
-            // setBudgetsUtilities(res.data[0].expenses)
-            // setBudgetsSubscription(res.data[0].expenses)
-            } else {
-                console.log("No Groceries")
-               
-            }
-  
-   
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    console.log("useEffect");
+    fetch("api/users/user", {
+      credentials: "include",
+    })
+      .then((res) => {
+        console.log(`response to authenticate ${res}`);
+        console.log(res, "yesyes");
+        return res.json(res);
       })
-        .catch(err => console.log(err));
+      .then((data) => {
+        console.log(data, "checkdata");
+        getValuesType(data);
+      })
+      .catch((err) => {
+        console.log("Error fetching authorized user.");
+      });
+  }, []);
 
-			})
-			.catch((err) => {
-				console.log('Error fetching authorized user.');
-			});
- 
+  function getValuesType(data) {
+    let userName = {
+      user: data.username,
+    };
+    console.log(userName, "username");
+    API.getExpensesbyType("Groceries", userName)
+      .then((res) => {
+        console.log("nada");
+        if (res) {
+            setbudgetsbyType(res.data)
+          console.log(res.data, "checking  groceries");
+          res.data.map((type) => {
+            console.log(type._id);
+            if (type._id === "groceries") {
+                totalGroceries =+ type.totalType;
+                console.log(totalGroceries)
+                setGroceries(type.totalType);
+                setTotal(setTotal + type.totalType)
+           
+            }
+            if (type._id === "utilities") {
+                totalGroceries =+ type.totalType;
+                console.log(totalGroceries)
+                setUtilities(type.totalType)
+                setTotal(setTotal + type.totalType)
+            };
+            if (type._id === "subscription") {
+                totalGroceries =+ type.totalType;
+                console.log(totalGroceries)
+                setSubscription(type.totalType);
+                setTotal(setTotal + type.totalType)
+            }
+            console.log(type.totalType);
+          });
+    
+        } else {
+          console.log("No Groceries");
+        }
+      })
+      .catch((err) => console.log(err));
+  }
 
- 
-  
-  
-  
-    }, [])
+  function formatDate(date) {
+    const dateArray = date.split("-");
+    const year = dateArray[0];
+    const month = dateArray[1];
+    const dayArray = dateArray[2].split("T");
+    const day = dayArray[0];
+    const formattedDate = [month, day, year].join("-");
+    return formattedDate;
+  }
 
+  return (
+    <div className="container">
+      <Row>
+        <Col size="md-12">
+          <h1 className="text-center">Reports</h1>
+        </Col>
 
-    function formatDate(date){
-        const dateArray = date.split("-");
-        const year = dateArray[0];
-        const month = dateArray[1];
-        const dayArray = dateArray[2].split("T");
-        const day = dayArray[0];
-        const formattedDate = [month, day, year].join("-");
-        return formattedDate
-    }
+        <Col size="md-12">
+          <h3 className="text-right ">Monthly Breakdown</h3>
+          <HR />
+        </Col>
+        <Col size="md-12">
+          <h3 className="text-right ">Charts</h3>
+          <HR />
+        </Col>
 
+        {/* Styled hr line */}
 
-
-    return (
-
-        <div className="container">
-            <Row>
-
-                <Col size="md-12">
-                    <h1 className="text-center">Reports</h1>
-                </Col>
-
-                <Col size="md-12">
-                    <h3 className="text-right ">Monthly Breakdown</h3>
-					<HR/>
-                </Col>
-
-
-{/* Dougnuts Column need to be a bit higher */}
-                <Col size="md-4">
-
-				<Doughnut
-                        data={{
-
-                            labels: ['Gas', 'Electric', 'Water', 'Internet'],
-                            datasets: [
-                                {
-                                    label: '#of Votes',
-                                    data: [12, 19, 3, 5],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)'
-                                    ],
-                                }
-                            ]
-                        }}
-                        height={300}
-                        width={250}
-                        options={{
-                            maintainAspectRatio: false,
-							legend:{
-
-							}
-
-                        }}
-                    />
-					
-                </Col>
-
-                <Col size="md-4">
-
-				<Doughnut
-                        data={{
-
-                            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                            datasets: [
-                                {
-                                    label: '#of Votes',
-                                    data: [12, 19, 3, 5, 2, 3],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)'
-                                    ],
-                                }
-                            ]
-                        }}
-                        height={300}
-                        width={250}
-                        options={{
-                            maintainAspectRatio: false,
-							legend:{
-
-							}
-
-                        }}
-                    />
-                </Col>
-
-                <Col size="md-4">
-				<Doughnut
-                        data={{
-
-                            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                            datasets: [
-                                {
-                                    label: '#of Votes',
-                                    data: [12, 19, 3, 5, 2, 3],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)'
-                                    ],
-                                }
-                            ]
-                        }}
-                        height={300}
-                        width={250}
-                        options={{
-                            maintainAspectRatio: false,
-							legend:{
-
-							}
-
-                        }}
-                    />
-                </Col>
-
-
-                <Col size="md-12">
-                    <h3 className="text-right ">Charts</h3>
-					<HR/>
-                </Col>
-
-                {/* Styled hr line */}
-
-                <Col size="md-12">
-                    <Bar
-                        data={{
-
-                            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                            datasets: [
-                                {
-                                    label: '#of Votes',
-                                    data: [12, 19, 3, 5, 2, 3],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)'
-                                    ],
-                                }
-                            ]
-                        }}
-                        height={300}
-                        width={250}
-                        options={{
-                            maintainAspectRatio: false,
-
-                        }}
-                    />
-                </Col>
-
-
-                <Col size="md-12">
-                    <h3 className="text-right ">Most Recent Expenses</h3>
-					<HR/>
-                </Col>
-
-                <Col size="md-12">
-					{/* Table need to be a little bit more up */}
-                    {budgets.map(budget => {
-                    return (
-                    <ListItem key={budget._id}>
-                      <a href={"/budgets/" + budget._id}>
-                        <strong>
-                        title : {budget.title} type: {budget.type} cost: {budget.cost} expires: {formatDate(budget.expires)}
-                        </strong>
-                      </a>
-                    </ListItem>
+        <Col size="md-12">
+          <Bar
+            data={{
+              labels: ["Groceries", "Utilities", "Subscription", "Total"],
+              datasets: [
+                {
+                  label: "Expenses",
+                  data: [budgetsGroceries, budgetsUtilities, budgetsSubscription, budgetsTotal],
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                  ],
+                },
+              ],
+            }}
+            height={300}
+            width={250}
+            options={{
+              maintainAspectRatio: false,
+            }}
+          />
+        </Col>
+        <Row>
+        {budgetsbyType.length ? (
+              <>
+                {budgetsbyType.map(typeGraph => {
+                percentage = Math.round((typeGraph.totalType / totalGroceries) * 100)
+                if (typeGraph._id === "groceries") back = ["rgba(255, 99, 132, 0.2)"]    
+                if (typeGraph._id === "utilities") back = ["rgba(54, 162, 235, 0.2)"] 
+                if (typeGraph._id === "subscription") back =  ["rgba(255, 206, 86, 0.2)"]
+                  return (
+                    <DoughnutChart key={typeGraph._id} total = {totalGroceries} labels1={typeGraph._id} data1={[typeGraph.totalType]} background1={back} border1={back}/>
                   );
                 })}
-                </Col>
-            </Row>
-        </div>
-
-
-
-
-    )
+                </>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+      
+      </Row> 
+        <Col size="md-12">
+          <h3 className="text-right ">Most Recent Expenses</h3>
+          <HR />
+        </Col>
+       
+        <Col size="md-12">
+          {/* Table need to be a little bit more up */}
+          {budgets.map((budget) => {
+            return (
+              <ListItem key={budget._id}>
+                <a href={"/budgets/" + budget._id}>
+                  <strong>
+                    title : {budget.title} type: {budget.type} cost:{" "}
+                    {budget.cost} expires: {formatDate(budget.expires)}
+                  </strong>
+                </a>
+              </ListItem>
+            );
+          })}
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-
-export default PublicRoute
+export default Reports;
