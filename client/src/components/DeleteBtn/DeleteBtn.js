353e9c26192fe3bd17da4f558e133393043da9a9
Alejandro Castro <castroal96@gmail.com>
2021-03-11 12:03:05 -0500

    added style to delete btn

--- client/src/components/DeleteBtn/DeleteBtn.js
+++ client/src/components/DeleteBtn/DeleteBtn.js
@@ -5,7 +5,7 @@ import "./DeleteBtn.css";
 // That way we don't have to define them all individually
 function DeleteBtn(props) {
   return (
-    <span className="delete-btn" {...props} role="button" tabIndex="0">
+    <span className="delete-btn text-dark font-weight-bold" {...props} role="button" tabIndex="0">
       ✗
     </span>
   );
